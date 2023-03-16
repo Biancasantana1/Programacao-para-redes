8aaadf296d9d411ee3e9e3bb036c6595621ac693
Antony Araujo <contato.antonyaraujo@gmail.com>
2023-03-16 04:27:35 -0300

    Passar dados capturados para arquivos csv

--- ProjetoProgRedes/main.py
+++ ProjetoProgRedes/main.py
@@ -1,6 +1,12 @@
 import requests
 import json
 
+def save_csv(data, titulo):
+    arquivo = open(titulo+'.csv', 'w')
+    arquivo.write(data)
+    arquivo.close()
+
+
 url = 'https://dadosabertos.camara.leg.br/api/v2/proposicoes'
 params = {
     'siglaTipo': 'PL',
@@ -17,6 +23,9 @@ else:
 
 data = json.loads(response.content)
 #print(data)
+
+autores_text = "nome;codTipo;Tipo;\n"
+projetos_text = "id_proposicao;ano;tipo;numero;assunto;autores;cod_situacao;\n"
 
 for prop in data['dados']:
     id_proposicao = prop['id']
@@ -41,10 +50,11 @@ for prop in data['dados']:
         nome = autor['nome']
         codTipo = autor['codTipo']
         tipoAutor = autor['tipo']
+        autores_text += nome+";"+str(codTipo)+";"+tipoAutor+";"+"\n"
         autores.append((nome,codTipo, tipoAutor))
-
+    
     print(id_proposicao, ano, tipo, numero, assunto, autores, cod_situacao)
+    projetos_text += str(id_proposicao) + ";" + str(ano) + ";" + str(tipo) + ";" + str(numero) + ";" + assunto + ";" + str(autores) + ";" + str(cod_situacao) + ";\n"
 
-
-
-
+save_csv(autores_text, 'autores')
+save_csv(projetos_text, 'projetos')
